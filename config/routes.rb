08f07dda860e9bf27da4221a8831c2d1a5f9bf01
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-03-23 14:41:15 +0900

    room削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
 
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
